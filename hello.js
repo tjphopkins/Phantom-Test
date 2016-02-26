3e41ff1154202a8b07a0109dd003dea6475e2b55
tjphopkins <tomjphopkins@gmail.com>
2016-02-26 12:07:24 +0000

    WIP - print analytics page with hard coded user

--- hello.js
+++ hello.js
@@ -1,2 +1,5 @@
-console.log("Hello, world!");
-phantom.exit();
+var page = require('webpage').create();
+page.open('http://localhost:8000/analytics/render/', function() {
+  page.render('analytics.pdf');
+  phantom.exit();
+});
