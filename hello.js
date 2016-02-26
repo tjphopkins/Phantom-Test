92579b74ee7cfb8abddf2ef70f4bdbe62d3fa3a0
tjphopkins <tomjphopkins@gmail.com>
2016-02-26 13:15:18 +0000

    WIP - add console messages

--- hello.js
+++ hello.js
@@ -1,5 +1,10 @@
 var page = require('webpage').create();
+
+page.onConsoleMessage = function(msg) {
+        console.log(msg);
+    };
+
 page.open('http://localhost:8000/analytics/render/', function() {
-  page.render('analytics.pdf');
-  phantom.exit();
+    page.render('analytics.pdf');
+    phantom.exit();
 });
