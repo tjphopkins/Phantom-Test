a34ac8d4d0c51729ac4fa38b0a07197bb3a9d6a1
tjphopkins <tomjphopkins@gmail.com>
2016-02-26 13:23:01 +0000

    WIP - exit on callback, inner requests failing - UNAUTHORISED

--- hello.js
+++ hello.js
@@ -4,7 +4,11 @@ page.onConsoleMessage = function(msg) {
         console.log(msg);
     };
 
+page.onCallback = function(data) {
+    console.log('CALLBACK: ' + JSON.stringify(data));
+    phantom.exit();
+};
+
 page.open('http://localhost:8000/analytics/render/', function() {
     page.render('analytics.pdf');
-    phantom.exit();
 });
